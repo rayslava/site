c6a371da01e661ffe3551c026376189ffd5cf2fd
Vyacheslav Barinov <v.barinov@samsung.com>
2013-07-22 10:02:41 +0400

    /about and /contacts are joined now

--- pages.lisp
+++ pages.lisp
@@ -73,6 +73,7 @@
 	    (:link :rel "stylesheet" :type "text/css" :href "/main.css")
 	    (:script :type "text/javascript" :src "/x-cl.js")
 	    (:script :type "text/javascript" :src "/jscl.js")
+	    (:script "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');")
 	    (:script :type "text/x-common-lisp" "(setf (cl::oget (#j:document:getElementById \"mail-addr\") \"innerHTML\") \"<a href=\\\"mailto:rayslava@gmail.com\\\"> rayslava@gmail.com<\/a>\")")
 	    (:meta :name "viewport" :content "initial-scale=1.0,maximum-scale=1.0,width=device-width,user-scalable=0"))
      (:body (:h2 "Contacts")
@@ -83,22 +84,8 @@
 		 (:li "Chat me via jabber: "
 		      (:a :href "xmpp:rayslava@jabber.ru?message;type=chat" "rayslava@jabber.ru"))
 		 (:li "Follow my personal " (:a :href "http://point.im" "Point.im") " blog at "
-		      (:a :href "xmpp:point@point.im?message;type=chat;body=S%20@rayslava" "point.im"))))))))
-
-(define-easy-handler (about-page :uri "/about"
-				 :default-request-type :get)
-    ()
-  (with-html-output-to-string (*standard-output* nil :prologue nil)
-    (:html
-     (:head (:title "About me")
-	    (:link :rel "stylesheet" :type "text/css" :href "/main.css")
-	    (:script :type "text/javascript" :src "/jscl.js")
-	    (:script "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');")
-	    (:meta :name "viewport" :content "initial-scale=1.0,maximum-scale=1.0,width=device-width,user-scalable=0;"))
-     (:body (:h2 "About me")
-	    (:p "I was born on the day when Morris Worm devoured ten percent of Internet and that predetermined my life&hellip;")
-	    (:p "I've graduated from BMSTU and now I work in Samsung Electronics Moscow")
-	    (:p "Several social profiles:"
+		      (:a :href "xmpp:point@point.im?message;type=chat;body=S%20@rayslava" "point.im"))))
+	    (:p "Also here are several social profiles:"
 		(:div :class "social-link"
 		      (:iframe
 		       :src "http://ghbtns.com/github-btn.html?user=rayslava&type=follow&count=true"
