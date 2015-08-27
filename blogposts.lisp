a49a0b27aa78b0025b618e1ca77b437ec4d81b64
Slava Barinov <rayslava@gmail.com>
2015-08-27 16:15:18 +0300

    Blog post typo fix

--- blogposts.lisp
+++ blogposts.lisp
@@ -6,7 +6,7 @@
 
 (defblogpost 3649655845 "Blog created"
   (:div
-   (:p "Okay, I've created a blog engline and will post something here.")
+   (:p "Okay, I've created a blog engine and will post something here.")
    (:p "As usual it's written in neat Common Lisp and can be seen at my "
        (:a :href "https://github.com/rayslava" "GitHub profile")))
   '("en" "site" "news" "lisp"))
