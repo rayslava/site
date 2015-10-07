99de2cd8467758fc5fb1272720b3352e36daa00d
Slava Barinov <rayslava@gmail.com>
2015-10-07 18:32:58 +0300

    Blog post on web design added

--- blogposts.lisp
+++ blogposts.lisp
@@ -4,7 +4,7 @@
 
 (in-package :piserv.blogposts)
 
-(defblogpost 3649655845 "Blog created"
+(defblogpost 3649655845 "On blog creation"
   (:div
    (:p "For the past years I tired of repeating myself in every conversation on certain
    subjects, so I'll just place my thoughts and reasoning here and provide links to
@@ -16,12 +16,12 @@
    (:p "Also if you know what RSS is you might be interested in subscribing "
        (:a :href "/rss" "the feed") "."))
   :tags '("en" "site" "news" "lisp")
-  :meta  ((:meta :property "og:title" :content "Blog created")
+  :meta  ((:meta :property "og:title" :content "On blog creation")
 	  (:meta :property "og:type" :content "blog")
 	  (:meta :property "og:description" :content "A note about blog creation")
 	  (:meta :property "og:url" :content "http://rayslava.com/blog?id=3649655845")))
 
-(defblogpost 3649996316 "Сделал блог"
+(defblogpost 3649996316 "О блоге"
   (:div
    (:p "Соорудил бложик, в котором буду записывать свои мысли по разным поводам. Просто
    надоело по нескольку раз писать одно и то же, когда в интернете кто-то неправ, поэтому
@@ -38,7 +38,112 @@
    (:p "Для особых людей, знающих, что такое RSS, сделана " (:a :href "/rss" "лента, на
    которую можно подписаться.")))
   :tags '("ru" "site" "news" "lisp")
-  :meta  ((:meta :property "og:title" :content "Сделал блог")
+  :meta  ((:meta :property "og:title" :content "О блоге")
 	  (:meta :property "og:type" :content "blog")
-	  (:meta :property "og:description" :content "Заметка о том, что у меня теперь есть бложик")
+	  (:meta :property "og:description" :content "Заметка о том, что у меня теперь
+	  есть бложик")
 	  (:meta :property "og:url" :content "http://rayslava.com/blog?id=3649996316")))
+
+(defblogpost 3653209732 "О веб-дизайне"
+  (:div
+   (:p "Глядя на современный веб мне становится грустно и печально, иногда  вплоть до
+   негодования: как мы могли из практически свободного сообщества инженеров скатиться в
+   то, что можно видеть сейчас – тотальную централизацию, засилье рекламы, абсолютную
+   компьютерную безграмотность пользователей.")
+   (:p "Впрочем, про это написано уже достаточно много статей и я не хочу повторяться,
+   этот пост про то, что я вижу на веб-страницах, как пользователь.")
+   (:p "Для начала замечу, что основным моим браузером течение рабочей недели является "
+       (:a :href "http://www.emacswiki.org/emacs/emacs-w3m" "emacs-w3m") ", и лишь когда не
+   удается что-то открыть в нем, с расстройством и матюгами запускаю firefox.")
+   (:p "Так вот последние годы я наблюдаю, как веб превращается в набор приложений,
+   написанных на javascript, с html5-интерфейсом, которые используют браузер, как средство
+   доставки. И это отвратительно.")
+   (:p "Я еще могу понять, когда корпорации, нацеленные на выкачивание денег из населения,
+   занимаются тем, что рисуют финтифлюшки и пытаются загнать как можно больше народу на
+   свои площадки, используя веселую ферму и фильтры для фотографий, превращающие
+   плохонькие картинки в настолько страшную мешанину цветов, что Мунк обзавидуется. Но
+   когда я пытаюсь открыть " (:strong "новостной сайт") ", чтобы посмотреть
+   одну-единственную новость и не могу этого сделать, потому что страница весит 3.5
+   мегабайта, требует поддержку cookie, javascript и еще какого-нибудь флеша, чтобы просто
+   отобразить три абзаца текста — я искренне и безмерно удивляюсь. Кому и зачем это
+   надо?")
+   (:p "Похоже, что массы убили почти всю суть веба, которая, как и все остальное
+   держалась на идеалистах, твердо соблюдающих идеи Тима Бернерса-Ли.")
+   (:p "В общем, мое сугубое мнение — работа должна выполняться на серверах, анимация не
+   нужна, js вреден, cookie можно выдавать в тех местах, где
+   пользователю " (:strong "необходимо авторизоваться") ", а не на каждом сайте просто для
+   того, чтобы насовать каждому как можно больше разной рекламы. Плюс, я иду в сеть за
+   информацией, а не за подачей, поэтому я все еще не совсем понимаю, для чего люди вешают
+   на свои страницы фоновые картинки по мегабайту и делают переливающиеся анимации по
+   углам. Остались, конечно, хорошие сайты, и ярчайший пример – Википедия. Абсолютно
+   простой прямой дизайн, информация представлена в том виде, в котором человек ожидает ее
+   получить, не нужно ни javascript ни cookie, форматирование присутствует на минимально
+   необходимом уровне, чтобы выделять реально важные места. Также отлично оформлены
+   страницы проекта GNU.")
+   (:p "Антипример — любая социальная сеть. Скажем facebook или, еще лучше, instagram.
+   Последний без javascript и cookie не позволяет даже посмотреть изображение. А с
+   поддержкой — не дает скопировать ссылку на это изображение. Приходится писать скрипты
+   для парсинга. И все это густо обвешано жуткого вида баннерами, наполнено рекламными
+   постами и ценность представляет чуть менее, чем никакую.")
+   (:p "В целом веб-дизайн по моему мнению дизайн должен быть как у "
+       (:a :href "https://gnu.org/" "GNU") " и "
+       (:a :href "https://wikipedia.org" "Wikipedia")
+   ": минималистичным, доносящим информацию, не зависящим от устройства и не делающим
+   лишнюю работу, чего я и стараюсь придерживаться на этом сайте.")
+   (:small "There is an " (:a :href "/blog?id=3653209733" "english version") " of this
+   post"))
+  :tags '("ru" "web" "design")
+  :meta  ((:meta :property "og:title" :content "О веб-дизайне")
+	  (:meta :property "og:type" :content "blog")
+	  (:meta :property "og:description" :content "Плач и стенания о том, как
+	  веб-дизайн катится в бездну ужаса")
+	  (:meta :property "og:url" :content "http://rayslava.com/blog?id=3653209732")))
+
+(defblogpost 3653209733 "On web design"
+  (:div
+   (:p "When looking on modern web I become very sad and sometimes even frustrated. How
+   could we make it from nearly free and independent community of engineers to the awful
+   thing we see now in browsers?")
+   (:p "Total centralization, advertisements everywhere, users don't have elementary
+   computer usage knowledge.")
+   (:p "However there are a lot of articles on this subject and I don't want to repeat
+   them. This post is about content I see on web-pages as a user.")
+   (:p "In the beginning I'll notice that my main web-browser is "
+       (:a :href "http://www.emacswiki.org/emacs/emacs-w3m" "emacs-w3m") " and only if it
+   can't handle something I launch firefox feeling very disappointed.")
+   (:p "And during the last several years I see how web mutates into a bunch of
+   applications written in js with html5 interface which use browser just as a delivery
+   system. And this is disgusting.")
+   (:p "I can understand when corporations targeted to getting money from users work on
+   creating jingles and whistles, online farms and creating photo filters which convert
+   not-so-good images into color mess so strange that Munch would envy badly. But when I
+   try to open a " (:strong "news site") " and can't see just a news because it's 3.5 MB,
+   requires js, cookie, flash and quad hd monitor just to show me three paragraphs of text
+   — I'm really surprised. Who and why does need that?")
+   (:p "It looks like masses kill the essence of web which was initially handled only by
+   idealists who were standing strong on Tim Berners-Lee ideas.")
+   (:p "So my opinion on web — the jobs must be done on server-side. Animation is not
+   needed, js harms, cookies may only be used in places where user " (:strong "must") " be
+   identified not everywhere just to spy the user and push him lots of new ads.")
+   (:p "I go to the net to get information, please give me my portion of information, not
+   mad designer fantasies. However there are still sites which are beautiful and one of
+   the most noticeable — Wikipedia. The information is represented in a way user want see
+   it, no need of js and cookies, formatting exists, but is very minimalistic and form
+   doesn't dominate on contents. The other good example is GNU project pages.")
+   (:p "Example of the opposite — any social network. Say facebook or even better —
+   Instagram. The latter won't allow you to see the image without js and won't allow to
+   copy image URL when you enable one. So the only way to see image is to write a
+   parser. And all of that is full of awful banners, filled up with advertising posts and
+   has nearly zero informational value.")
+   (:p "So web design should be just like it's done at "
+       (:a :href "https://gnu.org/" "GNU") " and "
+       (:a :href "https://wikipedia.org" "Wikipedia") " sites: minimalistic, clean and
+       don't do excessive things. And I'm trying to keep the site this way.")
+  (:small "There is a " (:a :href "/blog?id=3653209732" "russian version") " of this
+  post"))
+  :tags '("en" "web" "design")
+  :meta  ((:meta :property "og:title" :content "On web design")
+	  (:meta :property "og:type" :content "blog")
+	  (:meta :property "og:description" :content "Mourning the web design and the
+          whole web itself while looking on modern sites.")
+	  (:meta :property "og:url" :content "http://rayslava.com/blog?id=3653209733")))
