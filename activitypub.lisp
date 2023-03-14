76cfabb36d5e322f4811720514d7f283bd50209e
Slava Barinov <rayslava@gmail.com>
2023-03-14 23:46:27 +0300

    ActivityPub signature verification added

--- activitypub.lisp
+++ activitypub.lisp
@@ -41,6 +41,9 @@
 		       ("publicKeyPem" . ,site.config:*activitypub-public-key-pem*)))))))
 
 
+(defmacro fail (&body body)
+  `(throw :return (progn ,@body)))
+
 					; Accept followers
 (define-easy-handler (blog-inbox :uri "/ap/actor/blog/inbox"
 				 :default-request-type :post)
@@ -51,12 +54,67 @@
     (let ((request-type (hunchentoot:request-method hunchentoot:*request*)))
       (cond ((eq request-type :get) "" );; handle get request
             ((eq request-type :post)
-	     (print (format nil "Headers: ~% ~A~%" (headers-in hunchentoot:*request*)))
+	     ;;; Now we need to perform a signature verification
+	     (let* ((http-headers (headers-in hunchentoot:*request*))
+		    (signature-line (cdr (assoc :signature http-headers)))
+		    (lines (cl-ppcre:split "," signature-line))
+		    (line-parts (reduce 'nconc
+					(mapcar (lambda (l)
+						  (coerce
+						   (nth-value 1
+							      (cl-ppcre:scan-to-strings "(\\w*)=(.*\\\")$" l))
+						   'list))
+						lines)))
+		    (signature-parts
+		      (loop for (head . tail) on line-parts by #'cddr
+			    collect (cons head (car tail))))
+		    (keyid (string-trim "\"" (cdr (assoc "keyid" signature-parts :test #'string-equal))))
+		    (headers (string-trim "\"" (cdr (assoc "headers" signature-parts :test #'string-equal))))
+		    (signature (base64:base64-string-to-usb8-array
+				(string-trim "\"" (cdr (assoc "signature" signature-parts :test #'string-equal)))))
+		    (checked-headers (with-output-to-string (s)
+				       (mapcar #'(lambda (hdr)
+						   (format s "~%~A: ~A"
+							   hdr
+							   (cdr
+							    (assoc (intern (string-upcase hdr) :keyword)
+								   http-headers))))
+					       (cdr (cl-ppcre:split " " headers)))
+				       s))
+		    (message-to-check (concatenate 'string
+						   "(request-target): post /ap/actor/blog/inbox"
+						   checked-headers))
+		    (userprofile (cl-json:decode-json-from-string
+				  (handler-bind ((dex:http-request-gone #'(lambda (args)
+									    (fail
+									      (setf (return-code*) hunchentoot:+http-authorization-required+)
+									      (format nil "Can't load signature~A~%" args)))))
+				    (dex:get keyid :verbose t
+						   :headers
+						   '(("accept" . "application/ld+json; profile=\"http://www.w3.org/ns/activitystreams\""))))))
+		    (key (assoc :public-key userprofile))
+		    (pem (cdr (assoc :public-key-pem (cdr key))))
+		    (public-key (trivia:match
+				    (asn1:decode
+				     (base64:base64-string-to-usb8-array
+				      (cl-ppcre:regex-replace-all "\\n"
+								  (cl-ppcre:regex-replace-all ".*----\\n" pem "") "")))
+				  ((asn1:rsa-public-key-info n e)
+				   (ironclad:make-public-key :rsa :n n :e e)))))
+	       (unless
+		   (rsassa-pkcs1-v1_5-verify public-key
+					     (ironclad:ascii-string-to-byte-array message-to-check)
+					     signature
+					     :sha256)
+		 (fail
+		   (print (format nil "Signature verification failed~%"))
+		   (setf (return-code*) hunchentoot:+http-authorization-required+)
+		   (format nil "Signature verification failed~%"))))
+
              (let* ((data-string (hunchentoot:raw-post-data :force-text t))
                     (request-obj (cl-json:decode-json-from-string data-string)))
-	       (print (format nil "~A of type ~A~%" request-obj (cdr (assoc :type request-obj))))
+	       (print (format nil "~A of type ~A~%Checking signature~%" request-obj (cdr (assoc :type request-obj))))
 	       (when (string= "Follow" (cdr (assoc :type request-obj)))
-		 (print "Found follow")
 		 (send-signed (cdr (assoc :actor request-obj)) (generate-accept request-obj)))
 	       ""))))))
 
@@ -199,14 +257,13 @@
 	 (keyid "https://rayslava.com/ap/actor/blog#main-key"))
     (handler-bind ((dex:http-request-not-found #'dex:ignore-and-continue)
 		   (dex:http-request-not-implemented #'dex:ignore-and-continue))
-      (print
-       (dex:post reply-url :headers `(("content-type" . "application/ld+json")
+      (dex:post reply-url :headers `(("content-type" . "application/ld+json")
 					;				      ("@request-target" . ,(concatenate 'string "post " target-inbox))
-				      ("host" . ,target-domain)
-				      ("date" . ,date)
-				      ("digest" . ,hash)
-				      ("accept" . "application/ld+json; profile=\"http://www.w3.org/ns/activitystreams\"")
-				      ("Signature" . ,(generate-signed-header keyid target-inbox target-domain date hash)))
+				     ("host" . ,target-domain)
+				     ("date" . ,date)
+				     ("digest" . ,hash)
+				     ("accept" . "application/ld+json; profile=\"http://www.w3.org/ns/activitystreams\"")
+				     ("Signature" . ,(generate-signed-header keyid target-inbox target-domain date hash)))
 
-		 :content message
-		 :verbose t)))))
+		:content message
+		:verbose t))))
