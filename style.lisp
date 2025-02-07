40b8098623383294234bd5d84a9447442ce173ae
Slava Barinov <rayslava@gmail.com>
2025-02-07 21:59:53 +0900

    The blog style updates

--- style.lisp
+++ style.lisp
@@ -20,7 +20,11 @@
      ("a"
       :color "#000"
       ,@(transition 'color :timing-function "ease-in" :duration 0.2)
-      :transition-timing-function "linear")
+      :transition-timing-function "linear"
+      :text-decoration-thickness "0.3px;"
+      :-webkit-text-decoration-thickness "0.3px;"
+      :text-underline-offset "3px;"
+      :-webkit-text-underline-offset "3px;")
      ("a:hover" :color "#c00")
 
      ("@media screen and (max-width: 720px)"
@@ -33,7 +37,7 @@
        :display "block"))
      ("@media (prefers-color-scheme: dark)"
       ("body" :background "#1a1a1a" :color "#e0e0e0")
-      ("a" :color "#fff")
+      ("a" :color "#bbb")
       ("a:hover" :color "#ff6666")
       ("hr" :border-bottom "1px solid #404040")
       ("h1, h2, h3, h4" :color "#e0e0e0"))
@@ -55,11 +59,20 @@
      ("h1, h2, h3, h4"
       :margin "2.5rem 0 1.5rem 0"
       :line-height "1.25"
-      :color "#333")
+      :color "#777")
      ("#taglist" :padding "4px" :display "inline-block" :max-height "32px")
      (".tag" :padding "4px" :border "1px solid silver" :margin "4px"
 	     :border-radius "8px")
      ("#apubinfo" :padding "4px" :font-size "80%" :float "right")
      ("#timeinfo" :padding "4px" :font-size "80%" :float "right")
      (".text-with-image span" :display "inline-block" :height "1em" :width "auto" :vertical-align "middle")
-     (".text-with-image img" :height "100%" :width "auto" :display "block"))))
+     (".text-with-image img" :height "100%" :width "auto" :display "block")
+     (".year-split" :border-bottom "1px solid;"
+		    :border-image "linear-gradient(to right, #ccc, transparent) 1;"
+		    :margin-bottom "0.3rem;"
+		    :padding-bottom "0.2rem;")
+     (".year-split h3" :margin "0;"
+		       :color "#aaa;")
+     ("@media (prefers-color-scheme: dark)"
+      (".year-split h3" :color "#555;")
+      ("h1, h2, h3, h4" :color "#999")))))
