95c6d2a03b325701fd91543f8aaf32733c6d9228
Slava Barinov <rayslava@gmail.com>
2025-08-22 22:28:31 +0900

    chore: Cleanup the preview script

--- static/preview.lisp
+++ static/preview.lisp
@@ -1,128 +1,133 @@
-;; preview.lisp
+;;; preview.lisp
 ;;
 ;; Requires HTML elements like:
 ;;   (a :class "hover-preview" :data-preview "large.jpg" "Hover me")
-;; and an empty container:
-;;   (:div :id "preview-box")
+;; and an empty containers:
+;;   (:div :id "preview-box" :class "preview")
+;;   (:div :id "map-preview-box" :class "preview")
 
-;; preview.lisp — image + OSM, edge-aware placement
 
-(labels
-    ((px (n) (format nil "~Apx" n))
-     (num (s) (if (stringp s) (read-from-string s) s))
-     (embed-url (lat lng zoom)
-       (let* ((lat  (num lat))
-	      (lng  (num lng))
-	      (delta (case zoom
-		       ((17 18 19 20) 0.002)
-		       ((15 16)        0.004)
-		       ((13 14)        0.01)
-		       (t              0.02)))
-	      (min-lng (- lng delta))
-	      (min-lat (- lat delta))
-	      (max-lng (+ lng delta))
-	      (max-lat (+ lat delta)))
-	 (format nil
-		 "https://www.openstreetmap.org/export/embed.html?bbox=~S,~S,~S,~S&layer=mapnik&marker=~S,~S"
-		 min-lng min-lat max-lng max-lat lat lng)))
+(let* ((+hover-margin+	      30)   ; px gap from cursor
+       (+map-hide-delay-ms+   500)  ; delay before hiding the map box
+       (+default-map-zoom+    14)   ; default OSM zoom
+       (+coord-precision+     6))   ; decimal places for bbox/marker
 
-     ;; Position BOX near (mx,my). If it would overflow right/bottom edges,
-     ;; flip to the left/above respectively.
-     (place-box (box mx my &key (margin 30))
-       (let* ((vw (jscl::oget #j:window "innerWidth"))
-	      (vh (jscl::oget #j:window "innerHeight"))
-	      (bw (jscl::oget box "offsetWidth"))
-	      (bh (jscl::oget box "offsetHeight"))
-	      (left (if (> (+ mx margin bw) vw)
-			(- mx margin bw)
-			(+ mx margin)))
-	      (top  (if (> (+ my margin bh) vh)
-			(- my margin bh)
-			(+ my margin))))
-	 (let ((st (jscl::oget box "style")))
-	   (setf (jscl::oget st "left") (px left)
-		 (jscl::oget st "top")  (px top))))))
+  ;; Use macros so (setf (og ...)) stays a valid "place".
+  (macrolet ((og  (o k) `(jscl::oget ,o ,k))
+	     (st  (el)	`(og ,el "style"))
+	     (ds  (el)	`(og ,el "dataset")))
+    (labels
+	((px  (n) (format nil "~Apx" n))
+	 (num (s) (if (stringp s) (read-from-string s) s))
 
-  ;; -------------------------
-  ;; Image hover preview
-  ;; -------------------------
-  (let* ((preview (#j:document:getElementById "preview-box"))
-         (nodes   (#j:document:querySelectorAll ".hover-preview"))
-         (len     (jscl::oget nodes "length")))
-    (loop for i from 0 below len do
-      (let ((el (jscl::oget nodes i)))
-        ;; mouseenter: show and inject <img>
-        (setf (jscl::oget el "onmouseenter")
-              (lambda (e)
-                (let* ((ds  (jscl::oget el "dataset"))
-                       (src (jscl::oget ds "preview"))
-                       (mx  (jscl::oget e "pageX"))
-                       (my  (jscl::oget e "pageY")))
-                  (when src
-                    (setf (jscl::oget preview "innerHTML")
-                          (concatenate 'string "<img src=\"" src "\">"))
-                    ;; make visible so we can measure, then place near cursor (with edge flip)
-                    (let ((st (jscl::oget preview "style")))
-                      (setf (jscl::oget st "display") "block"))
-                    (place-box preview mx my)))))
-        ;; mousemove: follow cursor (with edge flip)
-        (setf (jscl::oget el "onmousemove")
-              (lambda (e)
-                (place-box preview (jscl::oget e "pageX") (jscl::oget e "pageY"))))
-        ;; mouseleave: hide
-        (setf (jscl::oget el "onmouseleave")
-              (lambda (e)
-                (setf (jscl::oget (jscl::oget preview "style") "display") "none"))))))
+	 (show!	    (el) (setf (og (st el) "display") "block"))
+	 (hide!	    (el) (setf (og (st el) "display") "none"))
+	 (set-html! (el html) (setf (og el "innerHTML") html))
 
-  ;; -------------------------
-  ;; OSM hover preview (iframe)
-  ;; -------------------------
-  (let* ((box        (#j:document:getElementById "map-preview-box"))
-         (nodes      (#j:document:querySelectorAll ".hover-map"))
-         (len        (jscl::oget nodes "length"))
-         (hide-timer nil))
-    ;; allow entering/leaving the map box without instant hide
-    (setf (jscl::oget box "onmouseenter")
-          (lambda (ev)
-            (when hide-timer (#j:window:clearTimeout hide-timer) (setf hide-timer nil))))
-    (setf (jscl::oget box "onmouseleave")
-          (lambda (ev)
-            (setf hide-timer
-                  (#j:window:setTimeout
-                   (lambda ()
-                     (setf (jscl::oget (jscl::oget box "style") "display") "none"))
-                   500))))
-    (loop for i from 0 below len do
-      (let ((el (jscl::oget nodes i)))
-        ;; show map on enter
-        (setf (jscl::oget el "onmouseenter")
-              (lambda (e)
-                (let* ((ds   (jscl::oget el "dataset"))
-                       (lat  (num (jscl::oget ds "lat")))
-                       (lng  (num (jscl::oget ds "lng")))
-                       (zoom (parse-integer (or (jscl::oget ds "zoom") "14")))
-                       (src  (embed-url lat lng zoom))
-                       (mx   (jscl::oget e "pageX"))
-                       (my   (jscl::oget e "pageY")))
-                  (setf (jscl::oget box "innerHTML")
-                        (concatenate 'string
-                                     "<iframe src=\"" src "\" loading=\"lazy\" "
-                                     "referrerpolicy=\"no-referrer-when-downgrade\" "
-                                     "style=\"width:100%;height:100%;border:0;display:block\"></iframe>"))
-                  (let ((st (jscl::oget box "style")))
-                    (setf (jscl::oget st "display") "block"))
-                  ;; place near cursor with edge flip
-                  (place-box box mx my)
-                  (when hide-timer (#j:window:clearTimeout hide-timer) (setf hide-timer nil)))))
-        ;; follow cursor with edge flip
-        (setf (jscl::oget el "onmousemove")
-              (lambda (e)
-                (place-box box (jscl::oget e "pageX") (jscl::oget e "pageY"))))
-        ;; schedule hide when leaving the link (unless entering box)
-        (setf (jscl::oget el "onmouseleave")
-              (lambda (e)
-                (setf hide-timer
-                      (#j:window:setTimeout
-                       (lambda ()
-                         (setf (jscl::oget (jscl::oget box "style") "display") "none"))
-                       500)))))))
+	 ;; Zoom → bbox delta (in degrees)
+	 (zoom->delta (zoom)
+	   (cond
+	     ((member zoom '(17 18 19 20))  0.002)
+	     ((member zoom '(15 16))	    0.004)
+	     ((member zoom '(13 14))	    0.01)
+	     (t				    0.02)))
+
+	 ;; Build OSM embed URL with fixed decimal formatting
+	 (embed-url (lat lng zoom)
+	   (let* ((lat	 (num lat))
+		  (lng	 (num lng))
+		  (d	 (zoom->delta zoom))
+		  (min-lng (- lng d))
+		  (min-lat (- lat d))
+		  (max-lng (+ lng d))
+		  (max-lat (+ lat d)))
+	     (flet ((f (x) (format nil "~,vs" +coord-precision+ x)))
+	       (format nil
+		       "https://www.openstreetmap.org/export/embed.html?bbox=~A,~A,~A,~A&layer=mapnik&marker=~A,~A"
+		       (f min-lng) (f min-lat) (f max-lng) (f max-lat) (f lat) (f lng)))))
+
+	 ;; Position BOX near (mx,my). If it would overflow right/bottom edges, flip.
+	 (place-box (box mx my &key (margin +hover-margin+))
+	   (let* ((vw (og #j:window "innerWidth"))
+		  (vh (og #j:window "innerHeight"))
+		  (bw (og box "offsetWidth"))
+		  (bh (og box "offsetHeight"))
+		  (left (if (> (+ mx margin bw) vw)
+			    (- mx margin bw)
+			    (+ mx margin)))
+		  (top	(if (> (+ my margin bh) vh)
+			    (- my margin bh)
+			    (+ my margin))))
+	     (let ((left (max 0 left)) (top (max 0 top)))
+	       (setf (og (st box) "left") (px left)
+		     (og (st box) "top")  (px top))))))
+
+      ;; -------------------------
+      ;; Image hover preview
+      ;; -------------------------
+      (let* ((preview (#j:document:getElementById "preview-box"))
+	     (nodes   (#j:document:querySelectorAll ".hover-preview"))
+	     (len     (og nodes "length")))
+	(loop for i from 0 below len do
+	  (let ((el (og nodes i)))
+	    (setf (og el "onmouseenter")
+		  (lambda (e)
+		    (let* ((src (og (ds el) "preview"))
+			   (mx	(og e "pageX"))
+			   (my	(og e "pageY")))
+		      (when src
+			(set-html! preview (concatenate 'string "<img src=\"" src "\">"))
+			(show! preview)
+			(place-box preview mx my)))))
+	    (setf (og el "onmousemove")
+		  (lambda (e)
+		    (place-box preview (og e "pageX") (og e "pageY"))))
+	    (setf (og el "onmouseleave")
+		  (lambda (e) (hide! preview))))))
+
+      ;; -------------------------
+      ;; OSM hover preview (iframe)
+      ;; -------------------------
+      (let* ((box	 (#j:document:getElementById "map-preview-box"))
+	     (nodes	 (#j:document:querySelectorAll ".hover-map"))
+	     (len	 (og nodes "length"))
+	     (hide-timer nil))
+	(setf (og box "onmouseenter")
+	      (lambda (ev)
+		(when hide-timer
+		  (#j:window:clearTimeout hide-timer)
+		  (setf hide-timer nil))))
+	(setf (og box "onmouseleave")
+	      (lambda (ev)
+		(setf hide-timer
+		      (#j:window:setTimeout (lambda () (hide! box))
+					    +map-hide-delay-ms+))))
+	(loop for i from 0 below len do
+	  (let ((el (og nodes i)))
+	    (setf (og el "onmouseenter")
+		  (lambda (e)
+		    (let* ((lat	 (num (og (ds el) "lat")))
+			   (lng	 (num (og (ds el) "lng")))
+			   (zoom (parse-integer (or (og (ds el) "zoom")
+						    (write-to-string +default-map-zoom+))))
+			   (src	 (embed-url lat lng zoom))
+			   (mx	 (og e "pageX"))
+			   (my	 (og e "pageY")))
+		      (set-html! box
+				 (concatenate 'string
+					      "<iframe src=\"" src "\" loading=\"lazy\" "
+					      "referrerpolicy=\"no-referrer-when-downgrade\" "
+					      "style=\"width:100%;height:100%;border:0;display:block\"></iframe>"))
+		      (show! box)
+		      (place-box box mx my)
+		      (when hide-timer
+			(#j:window:clearTimeout hide-timer)
+			(setf hide-timer nil)))))
+	    (setf (og el "onmousemove")
+		  (lambda (e)
+		    (place-box box (og e "pageX") (og e "pageY"))))
+	    (setf (og el "onmouseleave")
+		  (lambda (e)
+		    (setf hide-timer
+			  (#j:window:setTimeout (lambda () (hide! box))
+						+map-hide-delay-ms+))))))))))
